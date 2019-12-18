a3830e93c066dbab4144e6dab0b42d4bf14f7612
Adrien Chapelet <adrien3d@gmail.com>
2019-12-18 15:31:26 +0100

    Adding Google Analytics

--- static/js/add-on.js
+++ static/js/add-on.js
@@ -4,3 +4,12 @@
   is an official part of the theme (ex. Pull Requests) should be included in main.js
   and follow the formatting and style given.
 */
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-154750828-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-154750828-1');
+</script>
